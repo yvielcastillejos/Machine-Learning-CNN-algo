2268eda4a54509ba93503539853de7cf37fd19fa
casti136 <60524557+casti136@users.noreply.github.com>
2020-02-29 16:12:36 -0500

    Update sequential_function.py

--- Sequential_Function_Mnist/sequential_function.py
+++ Sequential_Function_Mnist/sequential_function.py
@@ -1,4 +1,3 @@
 def normalize(dataset_train,dataset_test):
   (dataset_train[0], dataset_train[1]), (dataset_test[0], dataset_test[1]) = (tf.keras.utils.normalize(x_train,axis =1), y_train), (tf.keras.utils.normalize(y_train,axis =1), y_train)
-  return 
   return dataset_train,dataset_test
