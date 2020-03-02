e784f2991c2fb1215362f64febe93e664bd79a55
yvielcastillejos <60524557+yvielcastillejos@users.noreply.github.com>
2020-03-01 21:54:09 -0500

    Update test_predictions.py

--- Sequential_Function_Mnist/test_predictions.py
+++ Sequential_Function_Mnist/test_predictions.py
@@ -1,19 +1,20 @@
-import matplotlib.pyplot as plt
+import tensorflow as tf
 import numpy as np
-import helloworldmi
+import matplotlib.pyplot as plt
 
-#save model
+#get data
+mnist = tf.keras.datasets.mnist
+(x_train, y_train), (x_test, y_test) = mnist.load_data()
 
-#print the accuracy and loss
-value_loss, value_acc = model.evaluate(x_test,y_test)
-print(value_loss,value_acc) #too much means underfit, too little overfit
+# normalize data
+(x_train, y_train), (x_test, y_test) = (tf.keras.utils.normalize(x_train, axis=1), y_train), (tf.keras.utils.normalize(x_test, axis=1), y_test)
 
-model.save("number_reader.model")
-new_model = tf.keras.models.load_model("number_reader.model)
+new_model = tf.keras.models.load_model("num_reader.model")
 predictions = new_model.predict(x_test)
 
-#prints 5 predictions of the images
-for i in range(0,5,1):
-  print("The prediction is: " + string(np.argmax.predictions[i]))
+for i in range(0,2,1):
+  print("The prediction is: ")
+  print(np.argmax(predictions[i]))
   print("The image is: ")
   plt.imshow(x_test[i])
+  plt.show()
